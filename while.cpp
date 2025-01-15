96d15d933c7a13733912286960782a76407d40dc
afzal75 <afzalbhuiyan16@gmail.com>
2025-01-15 13:39:55 +0600

    while loop intigration

--- while.cpp
+++ while.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+
 int main () {
-    
+    // counter
+    int counter = 1;
+    while (counter < 10) {
+        counter++;
+        cout <<counter << endl;
+        return 0;
+    }
 }
