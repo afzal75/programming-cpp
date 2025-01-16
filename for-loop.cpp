544c4dc18e7009f8823b6204d47c7b8878480826
afzal75 <afzalbhuiyan16@gmail.com>
2025-01-16 13:14:58 +0600

    for loop integration successfully

---
+++ for-loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int sum = 0;
+    for(int count = 1; count<= 10; count++) {
+        sum = sum + count;
+    }
+    return 0;
+}
--- while.cpp
+++ while.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
-
 int main () {
     // counter
     int counter = 1;
-    while (counter < 10) {
+    while (counter <= 5) {
+        cout <<counter;
         counter++;
-        cout <<counter << endl;
-        return 0;
     }
+    cout<< endl;
+    cout<< counter << endl;
+    counter = counter + 1;
+    counter++;
+    cout<< counter << endl;
+    return 0;
+
 }
